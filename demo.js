856dbe28b80e0252bebddd9770f040920c5771e1
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-10 11:22:03 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,94 +1,31 @@
 
+
+'use client'
+import axios from 'axios'
 import React, { Component } from 'react'
 
-export default class User extends Component {
+const URL='https://jsonplaceholder.typicode.com/users'
+export default class RestApp extends Component {
+    state={
+        userdata:[]
+    }
+    componentDidMount(){
+        axios.get(URL).then(response => response.data)
+        .then ((data) =>{
+              this.setState({
+                userdata:data
+              })
+        })
+    }
   render() {
     return (
-      <div>{this.props.users}
-      <button onClick={()=> this.props.userd(this.props.users)}>Delete</button>
+      <div>RestApp Example
+       {this.state.userdata.map((udata) =>(
+        <div key={udata.id}>
+            {udata.name} -- {udata.email}
+        </div>
+       ))}
       </div>
     )
-  }
-}
-
-
------------------
-
-
-
-
-import React, { Component } from 'react'
-import User from './User'
-
-export default class Users extends Component {
-  render() {
-    return (
-      <div>
-        {
-          this.props.ud.map((udata)=> <User key={udata} users={udata}
-          userd={this.props.deleteUser}
-          
-          />)
-        }
-       
-       <button disabled={this.props.hasData} onClick={this.props.da}>DeleteAll</button>
-      </div>
-    )
-  }
-}
-
-
-
--------
-
-"use client";
-
-import { Component } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Users from "./Users";
-import AddUser from "./AddUser";
-
-export default class MainApp extends Component {
-  state = {
-    headerData: "Welcome to Header",
-    userData: [],
-  };
-  deleteAll = () => {
-    this.setState(() => {
-      return {
-        userData: [],
-      };
-    });
-  };
-  deleteUser = (user) => {
-    this.setState((prevState) => {
-      return {
-        userData: prevState.userData.filter((option) => user != option),
-      };
-    });
-  };
-  addUser = (user) => {
-    this.setState((prevState) => {
-      return {
-        userData: prevState.userData.concat(user),
-      };
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Header hdata={this.state.headerData} />
-        <p>MainApp Comp</p>
-        <Users
-          ud={this.state.userData}
-          da={this.deleteAll}
-          hasData={!this.state.userData.length > 0}
-          deleteUser={this.deleteUser}
-        />
-        <AddUser au={this.addUser} />
-        <Footer />
-      </div>
-    );
   }
 }
