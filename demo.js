000f62c33216d4fbdabccccdfb24f6deabfacb49
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-10 11:58:02 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,31 +1,28 @@
+import MainApp from "./components/MainApp";
+import ServerComp from "./components/ServerComp";
+import RestApp from "./Rest/RestApp";
 
+async function productList() {
+  let data = await fetch("https://dummyjson.com/products");
+  data = await data.json();
+  return data.products;
+}
 
-'use client'
-import axios from 'axios'
-import React, { Component } from 'react'
-
-const URL='https://jsonplaceholder.typicode.com/users'
-export default class RestApp extends Component {
-    state={
-        userdata:[]
-    }
-    componentDidMount(){
-        axios.get(URL).then(response => response.data)
-        .then ((data) =>{
-              this.setState({
-                userdata:data
-              })
-        })
-    }
-  render() {
-    return (
-      <div>RestApp Example
-       {this.state.userdata.map((udata) =>(
-        <div key={udata.id}>
-            {udata.name} -- {udata.email}
-        </div>
-       ))}
+export default async function Home() {
+  let products = await productList();
+  console.log(products);
+  return (
+    <main>
+      <div>
+        <h2>Products</h2>
+        {products.map((item) => (
+          <div>
+            <h3>
+              Name :{item.title} ---{item.price}
+            </h3>
+          </div>
+        ))}
       </div>
-    )
-  }
+    </main>
+  );
 }
