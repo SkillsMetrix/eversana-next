b7f34ddce095c4494f221b0e14912b1ebe333878
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-15 13:58:33 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,47 +1,44 @@
+ import mongoose from "mongoose"
 
-'use client'
-import React, { useState } from 'react';
+const userModel= new mongoose.Schema({
+    name:String,
+    age:Number,
+    email:String
+})
+export const User= mongoose.model("users",userModel)
 
-function page(props) {
-    const [users,setUsers]= useState([])
-    const [name,setName]= useState('')
-    const [age,setAge]= useState('')
-    const [email,setEmail]= useState('')
 
-    const  addUser=async(e)=>{
-        e.preventDefault();
-        let response= await fetch('http://localhost:3000/api/users',{
-            method: "Post",
-            body:JSON.stringify( ...users,{name,age,email})
-        })
-        response= await response.json()
-        console.log(response);
-      setUsers([
-        //...users,{name,age,email}
- 
-      ])
- 
-    }
 
-    return (
-        <div>
-             <hr/>
-            <div>
-               {
-                users.map((data)=>(
-                    <div>{data.name}</div>
-                ))
-               }
 
-            </div>
-            <form onSubmit={addUser}>
-                UserName: <input value={name} onChange={(e)=> setName(e.target.value)}/>
-                Age: <input value={age} onChange={(e)=> setAge(e.target.value)}/>
-                Email: <input value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                <button type='submit'>Register</button>
-            </form>
-        </div>
-    );
+
+---
+    export const DBURL='mongodb+srv://amar:amar123@cluster0.rle5i.mongodb.net/mmcdb?retryWrites=true&w=majority&appName=Cluster0'
+
+
+
+----
+
+
+     import { DBURL } from "@/app/lib/dbconnect";
+import { User } from "@/app/lib/model/user";
+import { users } from "@/utils/db";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+
+
+export async function GET(){
+    //return new Response('Welcome to GET API')
+    //return NextResponse.json(users)
+    await mongoose.connect(DBURL)
+    const data= await User.find()
+    console.log(data);
+    return NextResponse.json({data})
 }
 
-export default page;
+export async function POST(request){
+    let payload =await request.json()
+    console.log(payload.name);
+    users.push(payload)
+    return NextResponse.json({result:"user created"})
+}
