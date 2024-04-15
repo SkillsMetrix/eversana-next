3d43fbcc3ad2ce407e0838a47c1581da7a8a6750
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-15 11:29:58 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,22 +1,21 @@
-import Link from "next/link";
+async function getUser(id) {
+    let data = await fetch(`http://localhost:3000/api/users/${id}`);
+    data = await data.json();
+    return data.result;
+  }
 
-async function getUsers() {
-  let data = await fetch("http://localhost:3000/api/users");
-  data = await data.json();
-  return data;
-}
-
-export default async function page() {
-  const users = await getUsers();
-  console.log(users);
-  return (
-    <div>
-      <h4>User List</h4>
-      {users.map((item) => (
-        <div>
-          <Link href={`api/users/${item.id}`}>{item.name}</Link>
+ 
+async function page({params}) {
+    console.log(params);
+    const user= await getUser(params.userid)
+    console.log(user);
+    return (
+<div>
+            <p>User Details</p>
+            <h4>User Age : {user.age}</h4>
+            <h4>User Email : {user.email}</h4>
         </div>
-      ))}
-    </div>
-  );
+    );
 }
+
+export default page;
